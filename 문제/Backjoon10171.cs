3657345fcab9b784769f82abdf62cdae528ddefd
Hye Rim <floweropq11@gmail.com>
2025-04-02 17:22:27 +0900

    백준 10171번 문제

---
+++ 문제/Backjoon10171.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace 사칙연산12
+{
+    class 고양이
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( \')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
