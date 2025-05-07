ebecf9534901eb57983e0fd77d2da67d4013a975
남혜림 <floweropq11@gmail.com>
2025-05-07 22:04:55 +0900

    Initial Commit

---
+++ c++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c++
+//
+//  Created by 남혜림 on 5/7/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
